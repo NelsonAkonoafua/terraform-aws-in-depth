9c94203ffcb1ae3cd2b1414660f4bb458e808f3e
Nelson Akonoafua <62257779+NelsonAkonoafua@users.noreply.github.com>
2025-10-29 09:41:32 +0100

    Create variables.tf
    This refers to the values of declared variables

---
+++ variables.tf
@@ -0,0 +1,20 @@
+variable "region" {
+  type = string
+  default = "us-east-1"
+}
+
+variable "bucket_name" {
+  type = string
+  default = "oppo-shop"
+}
+
+variable "env" {
+  type = string
+  default = "dev"
+}
+
+variable "resource" {
+  type = string
+  default = "not-here"
+}
+
