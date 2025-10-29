196f21fc386ec70de82a469da98d39d5bfd3656a
Nelson Akonoafua <62257779+NelsonAkonoafua@users.noreply.github.com>
2025-10-29 09:39:32 +0100

    Create main.tf
    This creates an EC2 instance and modules needed

---
+++ main.tf
@@ -0,0 +1,39 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.region
+
+}
+
+
+resource "aws_s3_bucket" "not-here" {
+  bucket = "oppo-shop"
+  force_destroy = true
+}
+
+resource "aws_s3_bucket_public_access_block" "not-here" {
+  bucket = aws_s3_bucket.not-here.id
+
+  block_public_acls       = false
+  block_public_policy     = true
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+module "s3_bucket_object" {
+  source      = "./modules/s3_bucket_object"
+  version     = ">= 0.12.0"
+  count       = 1
+  bucket_name = var.bucket_name
+  region      = var.region
+  env         = var.env
+  file_path   = "${path.module}/files/index.html"
+  file_key    = "index.html"
+} 
